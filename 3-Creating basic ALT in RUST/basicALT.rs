5c6178f7939f4fe2cc4574342185070b50997610
ZenX777 <139478072+ZenX777@users.noreply.github.com>
2023-10-30 14:47:14 +0530

    Add files via upload

---
+++ 3-Creating basic ALT in RUST/basicALT.rs
@@ -0,0 +1,25 @@
+// lib.rs
+use solana_program::{
+    account_info::next_account_info,
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    _input: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let _caller = next_account_info(accounts_iter)?;
+
+    // Implement your ALT logic here
+
+    msg!("ALT operation completed successfully");
+    Ok(())
+}
