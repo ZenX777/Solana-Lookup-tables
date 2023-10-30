4306f873a85bfbb92aad2871edc18f651d11a757
ZenX777 <139478072+ZenX777@users.noreply.github.com>
2023-10-30 15:00:25 +0530

    Delete 1-bash.sh

--- 1-bash.sh
+++
@@ -1,5 +0,0 @@
-#Start by setting up a new Solana project or using an existing one. 
-#You can initialize a new project using the following commands:
-
-$ solana init my_lookup_table_project
-$ cd my_lookup_table_project
