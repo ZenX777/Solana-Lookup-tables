088bd7b51df3e0c3d23843b2aff6dbff8c2c73bb
ZenX777 <139478072+ZenX777@users.noreply.github.com>
2023-10-30 15:01:10 +0530

    Add files via upload

---
+++ 1-CreateSolanaProject/1-bash.sh
@@ -0,0 +1,5 @@
+#Start by setting up a new Solana project or using an existing one. 
+#You can initialize a new project using the following commands:
+
+$ solana init my_lookup_table_project
+$ cd my_lookup_table_project
